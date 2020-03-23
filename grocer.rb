6ac35bd120a77b87494070678961b213253a9c58
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-23 17:39:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,6 +19,20 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  updated_cart = Array.new
+  index = 0
+
+  while index < cart.length do
+    current_item = cart[index]
+    if ( find_item_by_name_in_collection( current_item[:item], updated_cart ) == nil)
+      current_item[:count] = 1
+      updated_cart.push(current_item)
+    else
+      increment_count_of_item( updated_cart, current_item[:item] )
+    end
+    index += 1
+  end
+  updated_cart
 end
 
 def apply_coupons(cart, coupons)
